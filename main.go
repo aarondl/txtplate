88fbde5e5212d694cf58a5b37912e1e11e72c62a
Aaron L <aaron@bettercoder.net>
2017-11-23 15:47:36 -0800

    Add ability to use multiple files as input
    At the time of writing the following libraries did NOT work for this
    basic overriding merge:
    
    - github.com/VojtechVitek/mergemaps
    - github.com/ieee0824/go-deepmerge
    - github.com/imdario/mergo
    
    I have no idea how this is possible, but the code that I wrote works.

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"reflect"
 	"text/template"
 
 	yaml "gopkg.in/yaml.v2"
@@ -23,7 +24,7 @@ var (
 )
 
 var rootCmd = cobra.Command{
-	Use:   "txtplate [flags] <valuesfile>",
+	Use:   "txtplate [flags] <valuesfiles...>",
 	Short: "Apply values in a json or yaml file to go text/templated templates",
 	Long: `By default run stdin (or --input) through the go templating engine
 and output the result to stdout (or --output). Template functions available
@@ -40,7 +41,7 @@ func main() {
 	flags := rootCmd.Flags()
 	flags.StringVarP(&flagInput, "input", "i", "", "Input from the file given instead of stdin")
 	flags.StringVarP(&flagOutput, "output", "o", "", "Output from the file given instead of stdout")
-	rootCmd.Args = cobra.ExactArgs(1)
+	rootCmd.Args = cobra.MinimumNArgs(1)
 
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
@@ -52,32 +53,18 @@ func doTemplating(cmd *cobra.Command, args []string) error {
 	var byt []byte
 	var err error
 
-	byt, err = ioutil.ReadFile(args[0])
-	if err != nil {
-		return errors.Wrap(err, "failed to read values file")
-	}
-
-	var data interface{}
-
-	switch filepath.Ext(args[0]) {
-	case ".yaml", ".yml":
-		if err = yaml.Unmarshal(byt, &data); err != nil {
-			return errors.Wrap(err, "failed to parse values file as json")
-		}
-	default:
-		if err = json.Unmarshal(byt, &data); err != nil {
-			return errors.Wrap(err, "failed to parse values file as json")
-		}
-	}
-
 	if len(flagInput) != 0 {
 		byt, err = ioutil.ReadFile(flagInput)
 	} else {
 		byt, err = ioutil.ReadAll(os.Stdin)
 	}
-
 	if err != nil {
 		return errors.Wrap(err, "failed to read input")
+	}
+
+	data, err := readValuesFiles(args)
+	if err != nil {
+		return err
 	}
 
 	tpl, err := template.New("").Funcs(sprig.TxtFuncMap()).Parse(string(byt))
@@ -101,4 +88,105 @@ func doTemplating(cmd *cobra.Command, args []string) error {
 	}
 
 	return nil
+}
+
+func readValuesFiles(files []string) (interface{}, error) {
+	data := map[string]interface{}{}
+
+	for _, file := range files {
+		byt, err := ioutil.ReadFile(file)
+		if err != nil {
+			return nil, errors.Wrap(err, "failed to read values file")
+		}
+
+		var incomingData map[string]interface{}
+		switch filepath.Ext(file) {
+		case ".yaml", ".yml":
+			if err = yaml.Unmarshal(byt, &incomingData); err != nil {
+				return nil, errors.Wrapf(err, "failed to parse values file %s as yaml", file)
+			}
+			incomingData = convertToMapStringIntf(incomingData).(map[string]interface{})
+		default:
+			if err = json.Unmarshal(byt, &incomingData); err != nil {
+				return nil, errors.Wrapf(err, "failed to parse values file %s as json", file)
+			}
+		}
+
+		data, err = mergeMaps(data, incomingData)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return data, nil
+}
+
+// convertToMapStringIntf takes a object and recursively attempts to
+// convert any maps in it of type map[interface{}]interface{} to
+// map[string]interface{}, all other values are simply returned.
+func convertToMapStringIntf(value interface{}) interface{} {
+	switch m := value.(type) {
+	case map[string]interface{}:
+		for k, v := range m {
+			m[k] = convertToMapStringIntf(v)
+		}
+		return m
+	case map[interface{}]interface{}:
+		newMap := make(map[string]interface{}, len(m))
+		for k, v := range m {
+			newMap[k.(string)] = convertToMapStringIntf(v)
+		}
+		return newMap
+	default:
+		return value
+	}
+}
+
+var strMapType = reflect.TypeOf(map[string]interface{}{})
+
+// mergeMaps takes two map[string]interface{}
+// and attempts to merge them into dst. Keys that exist
+// in dst will be overwritten with values from src.
+func mergeMaps(dst, src interface{}) (map[string]interface{}, error) {
+	m, err := mergeMapsHelper(reflect.ValueOf(dst), reflect.ValueOf(src))
+	if err != nil {
+		return nil, err
+	}
+
+	return m.(map[string]interface{}), nil
+}
+
+func mergeMapsHelper(dst, src reflect.Value) (interface{}, error) {
+	if dst.Type() != strMapType {
+		return nil, errors.New("dst was not a map[string]interface{}")
+	}
+	if src.Type() != strMapType {
+		return nil, errors.New("src was not a map[string]interface{}")
+	}
+
+	for _, key := range src.MapKeys() {
+		srcValue := src.MapIndex(key).Elem()
+		dstValue := dst.MapIndex(key)
+		srcType := srcValue.Type()
+		var dstType reflect.Type
+
+		if dstValue.IsValid() {
+			dstValue = dstValue.Elem()
+			dstType = dstValue.Type()
+
+			if srcType == strMapType && dstType == strMapType {
+				intf, err := mergeMapsHelper(dstValue, srcValue)
+				if err != nil {
+					return nil, err
+				}
+
+				dst.SetMapIndex(key, reflect.ValueOf(intf))
+				continue
+			}
+		}
+
+		dst.SetMapIndex(key, srcValue)
+	}
+
+	return dst.Interface(), nil
 }
